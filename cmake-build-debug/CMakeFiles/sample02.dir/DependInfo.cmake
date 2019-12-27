08656809c746e44ebbc76af7cbbf81a893e4b2bf
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-12-27 17:35:43 +0900

    Apply transform

---
+++ cmake-build-debug/CMakeFiles/sample02.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jafffy/workspace/escena/lib/GLFWapp.cpp" "/home/jafffy/workspace/escena/cmake-build-debug/CMakeFiles/sample02.dir/lib/GLFWapp.cpp.o"
+  "/home/jafffy/workspace/escena/lib/scene_node.cc" "/home/jafffy/workspace/escena/cmake-build-debug/CMakeFiles/sample02.dir/lib/scene_node.cc.o"
+  "/home/jafffy/workspace/escena/lib/utils/shader.cpp" "/home/jafffy/workspace/escena/cmake-build-debug/CMakeFiles/sample02.dir/lib/utils/shader.cpp.o"
+  "/home/jafffy/workspace/escena/samples/sample02.cc" "/home/jafffy/workspace/escena/cmake-build-debug/CMakeFiles/sample02.dir/samples/sample02.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLFW_DLL"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sample02.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample02.dir/lib/GLFWapp.cpp.o"
+  "CMakeFiles/sample02.dir/lib/scene_node.cc.o"
+  "CMakeFiles/sample02.dir/lib/utils/shader.cpp.o"
+  "CMakeFiles/sample02.dir/samples/sample02.cc.o"
+  "sample02"
+  "sample02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
